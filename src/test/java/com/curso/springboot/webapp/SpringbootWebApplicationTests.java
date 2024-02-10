841891b65a2a2b7c4d52d44ec9180156cbf24831
Miguel Cortes <miguel.cortes@ias.com.co>
2024-02-10 11:04:00 -0500

    init project

---
+++ src/test/java/com/curso/springboot/webapp/SpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
